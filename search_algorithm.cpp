fa0ebdadc664e11c657efe85fa4557181200199b
Finn Klapper <realstraved@gmail.com>
2020-07-18 16:17:51 +0200

    Print the Board

--- search_algorithm.cpp
+++ search_algorithm.cpp
@@ -3,6 +3,16 @@
 using std::vector;
 using std::cout;
 
+void PrintBoard(vector<vector<int>> v1) {
+    for(vector<int> v : v1) {
+        for (int i = 0; i < v.size(); i++)
+        {
+            cout << v[i];
+        }
+        cout << "\n";
+    }
+}
+
 int main() {
     vector<vector<int>> board;
     board = {{0, 1, 0, 0, 0, 0},
@@ -10,4 +20,5 @@ int main() {
     {0, 1, 0, 0, 0, 0},
     {0, 1, 0, 0, 0, 0},
     {0, 0, 0, 0, 1, 0}};
+    PrintBoard(board);
 }
