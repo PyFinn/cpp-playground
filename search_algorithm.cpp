228025f403afb45dec92279c28fbc49f145d0490
Finn Klapper <realstraved@gmail.com>
2020-07-18 15:39:59 +0200

    Store a Grid

---
+++ search_algorithm.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using std::vector;
+using std::cout;
+
+int main() {
+    vector<vector<int>> board;
+    board = {{0, 1, 0, 0, 0, 0},
+    {0, 1, 0, 0, 0, 0},
+    {0, 1, 0, 0, 0, 0},
+    {0, 1, 0, 0, 0, 0},
+    {0, 0, 0, 0, 1, 0}};
+}
