8dd2ea5a1be0d417bf2de0ead895e39bcc64972a
Finn Klapper <realstraved@gmail.com>
2020-07-18 15:08:32 +0200

    Primitive Types

---
+++ primitive_types.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using std::cout;
+
+int main() {
+    int a = 10;
+
+    //Declaring a String variable without initializing right away.
+    std::string string1;
+
+    string1 = "Im a string";
+
+    cout << a << "\n";
+    cout << string1 << "\n";
+}
