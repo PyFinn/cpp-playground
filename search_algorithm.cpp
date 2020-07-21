7e07041a96e2bc80d1acdb53fbf0a352627c3fd0
Finn Klapper <realstraved@gmail.com>
2020-07-21 19:22:28 +0200

    Store the board using the state enum

--- search_algorithm.cpp
+++ search_algorithm.cpp
@@ -12,20 +12,26 @@ using std::istringstream;
 
 enum class State {kEmpty, kObstacle};
 
-vector<int> ParseLine(string toParse) {
-    vector<int> vctr;
+vector<State> ParseLine(string toParse) {
+    vector<State> vctr;
+    State curstate;
     istringstream stream1(toParse);
     int n;
     char c;
     while (stream1 >> n >> c)
     {
-        vctr.push_back(n);
+        if (n==0){
+          curstate = State::kEmpty;
+      }else if(n==1){
+          curstate = State::kObstacle;
+      }
+      vctr.push_back(curstate);
     }
     return vctr;
 }
 
-vector<vector<int>> ReadBoardFile(string path_to_file) {
-    vector<vector<int>> resultVector;
+vector<vector<State>> ReadBoardFile(string path_to_file) {
+    vector<vector<State>> resultVector;
     ifstream file1(path_to_file);
     if (file1)
     {
@@ -49,15 +55,16 @@ string CellString(State characterState) {
         return "⛰️ ";
         break;
     default:
+        return "0 ";
         break;
     }
 }
 
-void PrintBoard(vector<vector<int>> v1) {
-    for(vector<int> v : v1) {
+void PrintBoard(vector<vector<State>> v1) {
+    for(vector<State> v : v1) {
         for (int i = 0; i < v.size(); i++)
         {
-            cout << v[i];
+            cout << CellString(v[i]);
         }
         cout << "\n";
     }
