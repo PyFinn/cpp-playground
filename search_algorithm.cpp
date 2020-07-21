dd57f6331694962b774f50910d6f1254eee2696e
Finn Klapper <realstraved@gmail.com>
2020-07-21 18:29:37 +0200

    Astar Algorithm now able to read and format file input

--- search_algorithm.cpp
+++ search_algorithm.cpp
@@ -2,11 +2,40 @@
 #include <vector>
 #include <string>
 #include <fstream>
+#include <sstream>
 
 using std::vector;
 using std::cout;
 using std::ifstream;
 using std::string;
+using std::istringstream;
+
+vector<int> ParseLine(string toParse) {
+    vector<int> vctr;
+    istringstream stream1(toParse);
+    int n;
+    char c;
+    while (stream1 >> n >> c)
+    {
+        vctr.push_back(n);
+    }
+    return vctr;
+}
+
+vector<vector<int>> ReadBoardFile(string path_to_file) {
+    vector<vector<int>> resultVector;
+    ifstream file1(path_to_file);
+    if (file1)
+    {
+        cout << "File here" << "\n";
+        string line;
+        while (getline(file1, line))
+        {
+            resultVector.push_back(ParseLine(line));
+        }
+    }
+    return resultVector;
+}
 
 void PrintBoard(vector<vector<int>> v1) {
     for(vector<int> v : v1) {
@@ -17,29 +46,6 @@ void PrintBoard(vector<vector<int>> v1) {
         cout << "\n";
     }
 }
-
-void ReadBoardFile(string path_to_file) {
-    ifstream file1(path_to_file);
-    if (file1)
-    {
-        cout << "File here" << "\n";
-        string line;
-        while (getline(file1, line))
-        {
-            cout << line << "\n";
-        }
-        
-    }
-    
-}
-
 int main() {
-    vector<vector<int>> board;
-    board = {{0, 1, 0, 0, 0, 0},
-    {0, 1, 0, 0, 0, 0},
-    {0, 1, 0, 0, 0, 0},
-    {0, 1, 0, 0, 0, 0},
-    {0, 0, 0, 0, 1, 0}};
-    PrintBoard(board);
-    ReadBoardFile("file_to_read/1.board");
+    PrintBoard(ReadBoardFile("file_to_read/1.board"));
 }
