737f613a7a6870eaf15965cd0f419979c2ba9460
Finn Klapper <realstraved@gmail.com>
2020-07-22 08:43:38 +0200

    A-Star-Search

---
+++ .vscode/astar_search/a_search_algorithm.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+
+using std::vector;
+using std::cout;
+using std::ifstream;
+using std::string;
+using std::istringstream;
+
+enum class State {kEmpty, kObstacle};
+
+vector<State> ParseLine(string toParse) {
+    vector<State> vctr;
+    State curstate;
+    istringstream stream1(toParse);
+    int n;
+    char c;
+    while (stream1 >> n >> c)
+    {
+        if (n==0){
+          curstate = State::kEmpty;
+      }else if(n==1){
+          curstate = State::kObstacle;
+      }
+      vctr.push_back(curstate);
+    }
+    return vctr;
+}
+
+vector<vector<State>> ReadBoardFile(string path_to_file) {
+    vector<vector<State>> resultVector;
+    ifstream file1(path_to_file);
+    if (file1)
+    {
+        cout << "File here" << "\n";
+        string line;
+        while (getline(file1, line))
+        {
+            resultVector.push_back(ParseLine(line));
+        }
+    }
+    return resultVector;
+}
+
+string CellString(State characterState) {
+    switch (characterState)
+    {
+    case State::kEmpty:
+        return "0 ";
+        break;
+    case State::kObstacle:
+        return "⛰️ ";
+        break;
+    default:
+        return "0 ";
+        break;
+    }
+}
+
+vector<vector<State>> Search(vector<vector<State>> grid, int initialPoint[2], int goal_point[2]) {
+    cout << "No path found" << "\n";
+
+    return vector<vector<State>> {};
+}
+
+void PrintBoard(vector<vector<State>> v1) {
+    for(vector<State> v : v1) {
+        for (int i = 0; i < v.size(); i++)
+        {
+            cout << CellString(v[i]);
+        }
+        cout << "\n";
+    }
+}
+int main() {
+    auto board = ReadBoardFile("cpp_fundamentals/file_to_read/1.board");
+    int startPoint[2] = {0 , 0};
+    int endPoint[2] = {4 , 4};
+    vector<vector<State>> solution = Search(board, startPoint, endPoint);
+    PrintBoard(solution);
+}
