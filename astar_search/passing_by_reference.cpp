e1b282b9be03c6834b67a1d157a595c0115008f7
Finn Klapper <realstraved@gmail.com>
2020-07-28 14:41:24 +0200

    Fix

--- astar_search/passing_by_reference.cpp
+++ astar_search/passing_by_reference.cpp
@@ -3,8 +3,36 @@
 using std::cout;
 using std::string;
 
-int multiByTwoByValue(int)
+int multiByTwoByValue(int i) {
+    i = 2*i;
+    return i;
+}
+
+int MultiplyByTwo_passreference(int &i) {
+    i = 2*i;
+    return i;
+}
+
+int MultiplyByTwo_passpointer(int *i) {
+    *i = 2*(*i);
+    return *i;
+}
+
+void DoubleString(string &str) {
+    cout << str << " " << str << "\n";
+}
 
 int main() {
+    //Passing Value
+    cout << multiByTwoByValue(10) << "\n";
+    //Passing reference
+    int a = 10;
+    cout << MultiplyByTwo_passreference(a) << "\n";
+    //Passing pointer
+    int b = 10;
+    cout << MultiplyByTwo_passpointer(&b) << "\n";
+
     
+    string m = "Double me";
+    DoubleString(m);
 }
