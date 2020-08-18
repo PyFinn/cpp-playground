fd62525211e2ecaa2736c64a060b1bf4c958e944
PyFinn <realstraved@gmail.com>
2020-08-18 18:34:41 +0200

    Access management + Member Functions

--- object_oriented_cpp/structure.cpp
+++ object_oriented_cpp/structure.cpp
@@ -4,23 +4,31 @@ using std::cout;
 
 struct Date
 {
-    //Initialized variables with a default initialization
-    int day{7};
-    int month{6};
-    int year{2002};
+    public:
+        int Day() { return day; }
+        void setDay(int day) { this->day = day; }
+        int Month() { return month; }
+        void setMonth(int month) { this->month = month; }
+        int Year() { return year; }
+        void setYear(int year) { this->year = year; }
+    private:
+        //Initialized variables with a default initialization
+        int day{7};
+        int month{6};
+        int year{2002};
 };
 
 
 int main() {
     Date date;
 
-    date.day = 17;
-    date.month = 12;
-    date.year = 2020;
+    date.setDay(17);
+    date.setMonth(12);
+    date.setYear(2020);
 
-    assert(date.day > 0 && date.day <= 31);
-    assert(date.month > 0 && date.month <= 12);
-    assert(date.year <= 2020);
+    assert(date.Day() > 0 && date.Day() <= 31);
+    assert(date.Month() > 0 && date.Month() <= 12);
+    assert(date.Year() <= 2020);
 
-    cout << date.day << "." << date.month << "." << date.year << "\n";
+    cout << date.Day() << "." << date.Month() << "." << date.Year() << "\n";
 }
