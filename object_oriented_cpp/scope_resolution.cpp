61c0ce18b83798303f8d6ff75b11008533db58aa
PyFinn <realstraved@gmail.com>
2020-08-19 11:44:15 +0200

    Scope Res

---
+++ object_oriented_cpp/scope_resolution.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using std::cout;
+
+class Date {
+    public:
+        Date(int d, int m, int y);
+        int Day() {return day;}
+        int Month() {return month;}
+        int Year() {return year;}
+
+        void setDay(int day) {this->day = day;}
+        void setMonth(int month) {this->month = month;}
+        void setYear(int year) {this->year = year;}
+
+    private:
+        int day{1};
+        int month{1};
+        int year{0};
+};
+
+Date::Date(int d, int m, int y) {
+    setDay(d);
+    setMonth(m);
+    setYear(y);
+};
+
+int main() {
+    Date date(2,2,2);
+    cout << date.Day() << "." << date.Month() << "." << date.Year() << "\n";
+}
