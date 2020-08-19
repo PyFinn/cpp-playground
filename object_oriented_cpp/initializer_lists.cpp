ae4222d27e2cc44e6af8ff2a0ade9434abf42e3d
PyFinn <realstraved@gmail.com>
2020-08-19 15:40:29 +0200

    Const

--- object_oriented_cpp/initializer_lists.cpp
+++ object_oriented_cpp/initializer_lists.cpp
@@ -2,21 +2,23 @@
 using std::cout;
 
 class Date {
-    public:
-        Date(int d, int m, int y);
-        int Day() {return day;}
-        int Month() {return month;}
-        int Year() {return year;}
-    private:
-        int day{1};
-        int month{1};
-        int year{0};
+public:
+    Date(int d, int m, int y);
+    int Day() { return day; }
+    int Month() { return month; }
+    int Year() { return year; }
+
+private:
+    // Works with const
+    const int day{1};
+    const int month{1};
+    const int year{0};
 };
 
 //Initialize directly
 Date::Date(int d, int m, int y) : day(d), month(m), year(y) {}
 
 int main() {
-    Date date(2,2,2);
+    Date date(2, 2, 2);
     cout << date.Day() << "." << date.Month() << "." << date.Year() << "\n";
 }
